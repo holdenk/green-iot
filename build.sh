8bd03cf2bfd585fac7b2981343ad7c41b6a05dca
Holden Karau <holden@pigscanfly.ca>
2021-05-01 12:41:39 -0700

    Add a build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ ! -d venv ]; then
+   deactivate || echo "no venv active"
+   conda deactivate || echo "no conda active"
+   virtualenv venv --python python3
+fi
+source ./venv/bin/activate
+pip install -r requirements.txt   
+python manage.py collectstatic
+docker buildx -t holdenk/green-iot-web:latest --push  --platform linux/arm64,linux/amd64 . 
