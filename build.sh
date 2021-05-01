d870ae1a839d725d0079f006fd579f6b2e0ce053
Holden Karau <holden@pigscanfly.ca>
2021-05-01 13:54:34 -0700

    Delete the venv so we don't copy it into the dockerfile

--- build.sh
+++ build.sh
@@ -10,4 +10,6 @@ pip install -r requirements.txt
 rm -rf staticfiles
 # We set a bunch of params to null so we can run collect static
 EMAIL_HOST_USER='' EMAIL_HOST_PASSWORD='' STRIPE_SECRET_KEY='' STRIPE_PUBLISHABLE_KEY='' STRIPE_PLAN_MONTHLY_ID='' STRIPE_PLAN_ANNUAL_ID='' STRIPE_WEBHOOK_SIGNING_KEY='' SECRET_KEY='badsecret' EMAIL_HOST="" python manage.py collectstatic
+deactivate
+rm -rf venv
 docker buildx build -t holdenk/green-iot-web:latest --push  --platform linux/arm64,linux/amd64 . 
