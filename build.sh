07948789b4020c7e36b169778c079827d8fc0291
Holden Karau <holden@pigscanfly.ca>
2021-05-01 12:56:00 -0700

    Refresh staticfiles each build

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ if [ ! -d venv ]; then
    virtualenv venv --python python3
 fi
 source ./venv/bin/activate
-pip install -r requirements.txt   
+pip install -r requirements.txt
+rm -rf staticfiles
 # We set a bunch of params to null so we can run collect static
 EMAIL_HOST_USER='' EMAIL_HOST_PASSWORD='' STRIPE_SECRET_KEY='' STRIPE_PUBLISHABLE_KEY='' STRIPE_PLAN_MONTHLY_ID='' STRIPE_PLAN_ANNUAL_ID='' STRIPE_WEBHOOK_SIGNING_KEY='' SECRET_KEY='badsecret' EMAIL_HOST="" python manage.py collectstatic
 docker buildx build -t holdenk/green-iot-web:latest --push  --platform linux/arm64,linux/amd64 . 
